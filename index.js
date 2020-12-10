d6d95c975dff74833196ac53bc79736424a23c7b
Danny Haslund <dannyhasl1@gmail.com>
2020-12-10 12:47:20 +0100

    removing expo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { registerRootComponent } from 'expo';
+/* import { registerRootComponent } from 'expo';
 
 import App from './src/App';
 
@@ -6,3 +6,10 @@ import App from './src/App';
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
